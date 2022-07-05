76e736a0705e268e2383bc87323fa234e603eab1
Ana Beatriz Barbosa Daminelli <107115291+anabeatrizbd@users.noreply.github.com>
2022-07-05 16:42:06 -0300

    Update atv_2.java

--- atv_2.java
+++ atv_2.java
@@ -1,5 +1,21 @@
-package atv_2;
-
+import java.util.Scanner;
 public class atv_2 {
+    public static void main(String[] args) {
+       Scanner trapezio = new Scanner(System.in);
+        float h, bmenor, bmaior, area;
+
+        System.out.println("Digite a altura do trapézio: ");
+        h = trapezio.nextInt();
+        System.out.println("Digite a base menor do trapézio: ");
+        bmenor = trapezio.nextInt();
+        System.out.println("Digite a base maior do trapézio: ");
+        bmaior = trapezio.nextInt();
+
+        area = (h * (bmenor + bmaior)) / 2;
+            
+        System.out.println("A área exata é: " + area + " metros quadrados");
+        System.out.println("A área do trapézio arredondada é de: " + Math.round(a) + " metros quadrados");
+   
+    }
     
 }
