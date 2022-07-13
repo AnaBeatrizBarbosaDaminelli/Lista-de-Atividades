df392cf58a834bac5a6a8d50189bb3e406dad235
Ana Beatriz Barbosa Daminelli <107115291+anabeatrizbd@users.noreply.github.com>
2022-07-13 09:35:28 -0300

    Create atv_9.java

---
+++ atv_9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+    public class atv_9 {
+    public static void main (String[]args){
+        float n1, n2, media;
+        Scanner var = new Scanner (System.in);
+        System.out.println("Digite a primeira nota: ");
+        n1 = var.nextFloat();
+        System.out.println("Digite a segunda nota: ");
+        n2 = var.nextFloat();
+        media = (n1+n2)/2;
+    JOptionPane.showMessageDialog(null,"Média Final: " + media);
+    }
+}
