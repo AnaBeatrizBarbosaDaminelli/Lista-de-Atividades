5336528340e48ebf42522bee0073e9d7ca1644d3
Ana Beatriz Barbosa Daminelli <107115291+anabeatrizbd@users.noreply.github.com>
2022-07-14 15:56:19 -0300

    Update atv_10.java

--- atv_10.java
+++ atv_10.java
@@ -1,34 +1,34 @@
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 public class atv_10 {
-    public static void main(String[] args) {
-        String dia=JOptionPane.showInputDialog("Digite um número entre 1 a 7");
-        if (dia!= null) {
-            try{
-                int diasemana = Integer.parseInt(dia);
-                if (diasemana == 1)
-                    dia = "Domingo";
-                else if (diasemana == 2)
-                    dia = "Segunda";
-                else if (diasemana == 3)
-                        dia = "Terça";
-                else if (diasemana == 4)
-                        dia = "Quarta";
-                else if (diasemana == 5)
-                        dia = "Quinta";
-                else if (diasemana == 6)
-                        dia = "Sexta";
-                else if (diasemana == 7)
-                        dia = "Sábado";
-                else 
-                    dia = "Desconheço";
-                JOptionPane.showMessageDialog(parentComponent: null, dia);
-                } 
-            }
+    public static void main(String[] args){
+    String dia= JOptionPane.showInputDialog(null, "Digite um número entre 1 e 7: ");
+    if (dia != null) {
+    try{
+         int diasemana = Integer.parseInt(dia);
+          if (diasemana==1)
+              dia = "Domingo";
+          else if (diasemana==2)
+              dia = "Segunda-Feira";
+          else if (diasemana==3)
+              dia = "Terça-Feira";
+          else if (diasemana==4)
+              dia = "Quarta-Feira";
+          else if (diasemana==5)
+              dia = "Quinta-Feira";
+          else if (diasemana==6)
+              dia = "Sexta-Feira";
+          else if (diasemana==7)
+              dia = "Sábado";
+          else 
+              dia = "Dia inválido";                    
+                JOptionPane.showMessageDialog(null, dia);    
+            }    
 
-            catch(NumberFormatException erro){
-                JOptionPane.showMessageDialog(parentComponent:null, "Formato Inválido");
-            } 
-        } 
+     catch(NumberFormatException erro) {
+        JOptionPane.showMessageDialog(null, "Formato Inválido");
+
+                }
+            }    
+        }
     }
-}
